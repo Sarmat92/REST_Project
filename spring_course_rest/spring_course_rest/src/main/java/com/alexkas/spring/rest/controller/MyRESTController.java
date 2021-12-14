846d5ebbb86d12cc7ee5d514b96179c6dbf5d641
Sarmat92 <joedassin2015@gmail.com>
2021-12-14 22:10:09 +0300

    add method getEmployee

--- spring_course_rest/spring_course_rest/src/main/java/com/alexkas/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/spring_course_rest/src/main/java/com/alexkas/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.alexkas.spring.rest.entity.Employee;
 import com.alexkas.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,11 @@ public class MyRESTController {
 
     @GetMapping("/employee")
     public List<Employee> showAllEmployee() {
+        return employeeService.getAllEmployees();
+    }
 
-        List<Employee> allEmployees = employeeService.getAllEmployees();
-        return allEmployees;
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployee(id);
     }
 }
